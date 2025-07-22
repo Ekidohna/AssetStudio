ae81a5d8f3b2dbd3893bcbb897f47faadeeb8ae0
Ekidohna <Ekidohna@outlook.com>
2025-07-22 20:33:04 +0800

    去除了注释

--- AssetStudioGUI/Exporter.cs
+++ AssetStudioGUI/Exporter.cs
@@ -11,7 +11,7 @@ namespace AssetStudioGUI
     {
         public static bool ExportTexture2D(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported Texture2D.");
+            //System.Console.WriteLine("Exported Texture2D.");
             var m_Texture2D = (Texture2D)item.Asset;
             if (Properties.Settings.Default.convertTexture)
             {
@@ -41,7 +41,7 @@ namespace AssetStudioGUI
 
         public static bool ExportAudioClip(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported AudioClip.");
+            //System.Console.WriteLine("Exported AudioClip.");
             var m_AudioClip = (AudioClip)item.Asset;
             var m_AudioData = m_AudioClip.m_AudioData.GetData();
             if (m_AudioData == null || m_AudioData.Length == 0)
@@ -67,7 +67,7 @@ namespace AssetStudioGUI
 
         public static bool ExportShader(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported Shader.");
+            //System.Console.WriteLine("Exported Shader.");
             if (!TryExportFile(exportPath, item, ".shader", out var exportFullPath))
                 return false;
             var m_Shader = (Shader)item.Asset;
@@ -78,7 +78,7 @@ namespace AssetStudioGUI
 
         public static bool ExportTextAsset(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported TextAsset.");
+            //System.Console.WriteLine("Exported TextAsset.");
             var m_TextAsset = (TextAsset)(item.Asset);
             var extension = ".txt";
             if (Properties.Settings.Default.restoreExtensionName)
@@ -97,7 +97,7 @@ namespace AssetStudioGUI
 
         public static bool ExportMonoBehaviour(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported MonoBehaviour.");
+            //System.Console.WriteLine("Exported MonoBehaviour.");
             if (!TryExportFile(exportPath, item, ".json", out var exportFullPath))
                 return false;
             var m_MonoBehaviour = (MonoBehaviour)item.Asset;
@@ -114,7 +114,7 @@ namespace AssetStudioGUI
 
         public static bool ExportFont(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported Font.");
+            //System.Console.WriteLine("Exported Font.");
             var m_Font = (Font)item.Asset;
             if (m_Font.m_FontData != null)
             {
@@ -133,7 +133,7 @@ namespace AssetStudioGUI
 
         public static bool ExportMesh(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported Mesh.");
+            //System.Console.WriteLine("Exported Mesh.");
             var m_Mesh = (Mesh)item.Asset;
             if (m_Mesh.m_VertexCount <= 0)
                 return false;
@@ -216,7 +216,7 @@ namespace AssetStudioGUI
 
         public static bool ExportVideoClip(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported VideoClip.");
+            //System.Console.WriteLine("Exported VideoClip.");
             var m_VideoClip = (VideoClip)item.Asset;
             if (m_VideoClip.m_ExternalResources.m_Size > 0)
             {
@@ -230,7 +230,7 @@ namespace AssetStudioGUI
 
         public static bool ExportMovieTexture(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported MovieTexture.");
+            //System.Console.WriteLine("Exported MovieTexture.");
             var m_MovieTexture = (MovieTexture)item.Asset;
             if (!TryExportFile(exportPath, item, ".ogv", out var exportFullPath))
                 return false;
@@ -240,7 +240,7 @@ namespace AssetStudioGUI
 
         public static bool ExportSprite(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported Sprite.");
+            //System.Console.WriteLine("Exported Sprite.");
             var type = Properties.Settings.Default.convertType;
             if (!TryExportFile(exportPath, item, "." + type.ToString().ToLower(), out var exportFullPath))
                 return false;
@@ -261,7 +261,7 @@ namespace AssetStudioGUI
 
         public static bool ExportRawFile(AssetItem item, string exportPath)
         {
-            System.Console.WriteLine("Exported RawFile.");
+            //System.Console.WriteLine("Exported RawFile.");
             if (!TryExportFile(exportPath, item, ".dat", out var exportFullPath))
                 return false;
             File.WriteAllBytes(exportFullPath, item.Asset.GetRawData());
@@ -270,7 +270,7 @@ namespace AssetStudioGUI
 
         private static bool TryExportFile(string dir, AssetItem item, string extension, out string fullPath)
         {
-            System.Console.WriteLine("TryExportFile.");
+            //System.Console.WriteLine("TryExportFile.");
             var fileName = FixFileName(item.Text);
             fullPath = Path.Combine(dir, fileName + extension);
             if (!File.Exists(fullPath))
